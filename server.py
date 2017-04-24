db7aa265795a4e58e088363713e62d36a2881a41
Aarnav Bos <aarnavbos@gmail.com>
2017-04-24 09:33:01 +0530

    pushing to server for test

--- server.py
+++ server.py
@@ -105,6 +105,5 @@ def delete_note(id):
 
 
 
-if __name__ == "__main__":
-	configure_uploads(app, photos)	
-	app.run(debug=True,host='0.0.0.0',port=5000)
+if __name__ == "__main__":	
+	app.run()
---
+++ static/service-worker.js
@@ -0,0 +1,6 @@
+//Service-Worker-Allowed: "/" 
+
+self.addEventListener( "fetch" , function (event) {
+    //fetch request as specified by event object 
+    console.log(event.request); //Note that Request and Response are also objects 
+});
